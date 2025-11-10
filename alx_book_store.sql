825da92d27bb526c8f1f377d7ed6af2fa736c962
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-11-10 11:36:00 +0300

    final

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -24,7 +24,7 @@ CREATE TABLE alx_book_store.Orders
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
-CREATE TABLE alx_book_store.order_details(
+CREATE TABLE alx_book_store.order_Details(
     orderdetailid INT PRIMARY KEY,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id),
