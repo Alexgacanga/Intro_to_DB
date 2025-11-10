4c7de0b1b9aa7a1295f8bcd98245c5f8feed9025
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-11-10 11:23:51 +0300

    Final

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,32 +1,33 @@
-CREATE DATABASE IF NOT EXISTS alx_book_store
-
-create table alx_book_store.Authors(
-author_id int primary key,
-author_name varchar(215));
-
-
-
-create table alx_book_store.Books(
-book_id int primary key,
-title varchar(130),
-foreign key (book_id) references Authors(author_id),
-price double,
-publication_date date);
-
-create table alx_book_store.Customers(
-customer_id int primary key,
-customer_name varchar(215),
-email varchar(215),
-address text);
-
-create table alx_book_store.Orders(
-order_id int primary key,
-foreign key(order_id) references Customers(customer_id),
-order_date date);
-
-
-create table alx_book_store.order_details(
-orderdetailid int primary key,
-foreign key (orderdetailid) references Orders(order_id),
-foreign key (orderdetailid) references Books(book_id),
-quantity double);
+CREATE DATABASE
+IF NOT EXISTS alx_book_store
+CREATE TABLE alx_book_store.Authors
+(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+CREATE TABLE alx_book_store.Books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    FOREIGN KEY (book_id) REFERENCES Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
+);
+CREATE TABLE alx_book_store.Customers
+(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+CREATE TABLE alx_book_store.Orders
+(
+    order_id INT PRIMARY KEY,
+    FOREIGN KEY(order_id) REFERENCES Customers(customer_id),
+    order_date DATE
+);
+CREATE TABLE alx_book_store.order_details(
+    orderdetailid INT PRIMARY KEY,
+    FOREIGN KEY (orderdetailid) REFERENCES Orders(order_id),
+    FOREIGN KEY (orderdetailid) REFERENCES Books(book_id),
+    quantity DOUBLE
+);
