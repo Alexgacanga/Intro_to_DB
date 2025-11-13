23cea83c088afc0fdd88e2ace5ed38964caa0f97
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-11-13 13:16:41 +0300

    Final

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE IF NOT EXISTS alx_book_store
-USE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE alx_book_store.Authors
 (
     author_id INT PRIMARY KEY,
